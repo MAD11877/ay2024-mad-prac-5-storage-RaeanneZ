db86ab68a5378c4f67fbbbf3342dd5b8abd7b082
Raeanne <raeannezou@gmail.com>
2024-06-24 09:15:32 +0800

    Completed practical 5

---
+++ app/src/main/java/sg/edu/np/mad/practical5/UserAdapter.java
@@ -0,0 +1,54 @@
+package sg.edu.np.mad.practical5;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+    private List<User> userList;
+
+    public UserAdapter(List<User> userList, ListActivity activity) {
+        this.userList = userList;
+    }
+
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_list, parent, false);
+        UserViewHolder holder = new UserViewHolder(view);
+        return new UserViewHolder(view);
+    }
+
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.nameView.setText(user.name);
+        holder.descriptionView.setText(user.description);
+
+        // Set a placeholder image for the profile.
+        holder.smallImgView.setImageResource(R.drawable.ic_launcher_foreground);
+        holder.bigImgView.setImageResource(R.drawable.ic_launcher_foreground);
+
+    }
+
+    public int getItemCount() {
+        return userList.size();
+    }
+
+}
